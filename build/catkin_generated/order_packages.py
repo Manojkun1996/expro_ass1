2a288788515f51d9c063dcc92e6a89a63c1fc007
Manoj Kunapalli <manoj.bemt.14@gmail.com>
2021-07-31 15:45:28 +0000

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/root/Desktop/expro_ass1/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/Expro_Ass1/devel;/home/ass/devel;/home/armor_ws/devel;/home/course_ws/devel;/home/tmp_ws/devel;/opt/ros/kinetic".split(';') if "/home/Expro_Ass1/devel;/home/ass/devel;/home/armor_ws/devel;/home/course_ws/devel;/home/tmp_ws/devel;/opt/ros/kinetic" != "" else []
